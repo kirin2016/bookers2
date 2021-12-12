f0e2777db7dd8c8fd793f755b929a7aacf0f57ca
EC2 Default User <ec2-user@ip-172-31-62-217.ec2.internal>
2021-12-12 21:51:02 +0000

    [ADD]テスト段階

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,62 @@
 class BooksController < ApplicationController
-  
+
+  before_action :correct_user, only: [:edit, :update]
+
+
   def create
     @books = Book.all
-    @book = Book.index(books_params)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def show
+    @book = Book.new
+    @book_detail = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
-  
-  
+
+
   private
 
-  def bookes_params
-    params.require(:books).permit(:title, :body)
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
-  
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '35f2b66a082f8be220893f624b1333c21fa45ce6bebbf36d70f30b4af3aa7fa21564a3d6018969c75076168291af6419be8b6debea04add048d9220174500782'
